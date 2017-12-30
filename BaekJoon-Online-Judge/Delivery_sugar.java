93cb10382c4e4ec1571bb109d934e8d5f4fe600e
In9Kim <34940219+In9Kim@users.noreply.github.com>
2017-12-30 21:31:31 +0900

    Update Delivery_sugar.java

--- BaekJoon-Online-Judge/Delivery_sugar.java
+++ BaekJoon-Online-Judge/Delivery_sugar.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Main {
+public class Delivery_sugar {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		int A = input.nextInt();
